8cf2557309f69c952dc6fadb872e21a7eb5d63ea
Timm Nicolaizik <timmton@gmail.com>
2021-04-13 21:49:39 +0200

    Username is now sent with login

--- backend/routes.js
+++ backend/routes.js
@@ -111,10 +111,10 @@ router.post('/login', (req,res)=>{
             }
             else{
                 res.set('X-Result', 'WARNING')
-                res.status(200).json({'warning': 'Email not confirmed yet'})
+                res.status(200).json({'warning': 'Email not confirmed yet','username':'', 'profilepicture': ''})
                 return;
             }
-            res.status(200).json({})
+            res.status(200).json({'username':results[0].username, 'profilepicture': ''})
             return;
         });
     })
