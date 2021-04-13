868563e79eb14522ae79b80ac195eb1630413bbf
Timm Nicolaizik <timmton@gmail.com>
2021-04-13 16:59:03 +0200

    Revert "Revert "Added Google Analytics""
    This reverts commit 741f2c03808020ffd554636a2c6aeddaf56ddc5f.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+
+//Google Analytics
+Vue.use(VueAnalytics, {
+  id: 'UA-xxxxxxxxx-x',
+  router
+});
+
 
 new Vue({
   router,
