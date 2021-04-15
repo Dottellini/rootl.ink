41ca5ce184761049bfa222a69250ff25af06b953
Dottellini <kevinklauser@aol.de>
2021-04-15 18:04:45 +0200

    base64 image

--- src/components/EditAccountDetails.vue
+++ src/components/EditAccountDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <input id="imageInput" type="file" name="avatar">
+    <input id="imageInput" type="file" name="avatar" @change="ConvertFileTo('image/jpeg', $event)">
     <button type="button" @click="upload">Upload</button>
     <input id="username" type="text">
     <button>Save</button>
@@ -11,23 +11,49 @@
 export default {
   name: "EditAccountDetails",
 
+  data() {
+    return {
+      file: ""
+    }
+  },
+
   mounted() {
     document.getElementById("username").value = this.$store.state.account_username
   },
 
   methods: {
+    ConvertFileTo: function(outputMimeType, evt) {
+      new Promise(res => {
+        const file = evt.target.files[0];
+        console.log(file.size);
+        const reader = new FileReader();
+        reader.addEventListener('load', (content) => {
+          const img = new Image();
+          img.src = content.target.result.toString();
+          const canvas = document.getElementById('imageCanvas');
+          img.addEventListener('load', (e) => {
+            const ctx = canvas.getContext("2d");
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            canvas.height = img.naturalHeight;
+            canvas.width = img.naturalWidth;
+            ctx.drawImage(img, 0, 0);
+            this.file = canvas.toDataURL(outputMimeType, 0.7)
+          });
+        });
+        reader.readAsDataURL(file)
+      });
+    },
+
     upload: function () {
       let input = document.getElementById("imageInput")
       if(input.files.length === 0) { return }
-      console.log(input.files[0])
 
-      let f = (URL || webkitURL).createObjectURL(input.files[0]);
-      //let form = new FormData();
-      //form.append("file", f)
+      let data = {image: this.file}
 
       fetch('/uploadProfilePicture', {
         method: 'POST',
-        body: {avatar: f}
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
       }).then(response => {
         console.log(response)
       }).catch(err => {
