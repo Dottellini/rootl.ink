5333c3d116ad543eb0a62b262a41558fbfe9fc8a
Dottellini <kevinklauser@aol.de>
2021-04-16 12:31:42 +0200

    meh was vergessen

--- src/store/index.js
+++ src/store/index.js
@@ -8,14 +8,14 @@ export default new Vuex.Store({
   state: {
     list: [],
     username: "USERNAME", //Used for the "User-pages" and other stuff. This username if not to be used for the logged in account as it may change
-    account_username: "Alfredo", //Used for logged in user
+    account_username: null, //Used for logged in user
     profile_picture: "https://de.meming.world/images/de/thumb/b/bc/Mike_Wazowski-Sulley_Face_Swap.jpg/300px-Mike_Wazowski-Sulley_Face_Swap.jpg", //Used for UserPages
     account_profile_picture: "https://pbs.twimg.com/profile_images/949787136030539782/LnRrYf6e.jpg", //Used for logged in user
     background_hex: "#FFFFFF",
     text_hex: "#FFFFFF",
     linkBox_hex: "#000000",
     rootLink_hex: "#000000",
-    isLoggedIn: true,
+    isLoggedIn: false,
     isMobile: false,
   },
   mutations: {
