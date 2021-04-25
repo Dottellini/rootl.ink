3d910792335b7eff6fac3428c462b52fbeae85db
Timm Nicolaizik <timmmmnn@gmail.com>
2021-04-25 11:07:55 +0200

    Added QrCodeGeneration

--- src/views/qr.vue
+++ src/views/qr.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={{this.value}}">
+  <div id="QrCode">
+    <script type="text/javascript" src="../qrcode-gen/GenerateQrCode.js"></script>
   </div>
 </template>
 
@@ -9,13 +9,17 @@
   export default {
     data() {
       return {
-        value: 'https://example.com',
-        size: 300,
+        value: 'test',
         QRCode: undefined
       }
     },
     components: {
     },
+    mounted: {
+      function(){
+        GenerateQrCode("value",20)
+      }
+    }
   }
 </script>
 
