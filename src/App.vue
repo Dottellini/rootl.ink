617e858ad8c135b6086ce9b093dcca92df470eea
Timm Nicolaizik <timmmmnn@gmail.com>
2021-04-28 16:12:48 +0200

    Added ClickyAnalytics and Hotjar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
   <div id="app">
+    <script type="application/javascript">var clicky_site_ids = clicky_site_ids || []; clicky_site_ids.push(101313184);</script>
+    <script type="application/javascript" async src="//static.getclicky.com/js"></script>
+    <script type="application/javascript">
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:2350144,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
     <div class="content">
       <div id="header" class="header" v-if="!isUserPage">
         <div id="logo">
